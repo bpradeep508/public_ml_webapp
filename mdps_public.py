083252970a23574410f2c9ce2e12f44ad88b2a07
bpradeep508 <46367140+bpradeep508@users.noreply.github.com>
2023-09-14 12:55:04 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -39,10 +39,10 @@ if (selected == 'Flower Prediction using EKNN'):
     st.header('User Input Parameters')
 
     def user_input_features():
-        sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-        sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-        petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-        petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+        sepal_length = st.slider('Sepal length', 4.3, 7.9, 5.4)
+        sepal_width = st.slider('Sepal width', 2.0, 4.4, 3.4)
+        petal_length = st.slider('Petal length', 1.0, 6.9, 1.3)
+        petal_width = st.slider('Petal width', 0.1, 2.5, 0.2)
         data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
             'petal_length': petal_length,
